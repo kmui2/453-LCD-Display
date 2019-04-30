e7e3752cab9e62502f11aa91557ff73a0bd8a1d4
Kevin Mui <kmui2@wisc.edu>
2019-04-30 05:39:48 -0500

    brushed up simon says

--- src/renderer/components/SimonSays.vue
+++ src/renderer/components/SimonSays.vue
@@ -2,7 +2,19 @@
   <!-- TODO: add condition to select background color -->
   <div v-bind:style="{ backgroundColor: color || 'white' }" class="container">
     <header-title title="Simon Says"></header-title>
-    <h1 v-if="color === undefined">Ready?</h1>
+    <div v-if="color === undefined" class="cond">
+      <h1 class="title">It's Simon Says!</h1>
+      <h1>Ready?</h1>
+      <div class="gif">
+        <img src="../assets/good.gif">
+      </div>
+    </div>
+    <div v-if="color === 'white'" class="cond">
+      <h1>Now it's your turn!</h1>
+      <div class="gif">
+        <img src="../assets/trump_pointing.jpg">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,8 +35,34 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  font-size: 4em;
+  background: rgb(131, 58, 180);
+  background: linear-gradient(
+    90deg,
+    rgba(131, 58, 180, 1) 0%,
+    rgba(253, 29, 29, 1) 50%,
+    rgba(252, 176, 69, 1) 100%
+  );
+}
 .container {
   width: 100vw;
   height: 100vh;
+}
+.gif {
+  display: flex;
+}
+
+.gif > img {
+  margin: auto;
+}
+
+h1 {
+  text-align: center;
+}
+
+.cond {
+  display: flex;
+  flex-direction: column;
 }
 </style>
